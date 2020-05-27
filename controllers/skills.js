00bb2f7ba6375384411cb330f26d6343973a17bf
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-05-26 22:07:46 -0600

    All functionality works

--- controllers/skills.js
+++ controllers/skills.js
@@ -12,7 +12,36 @@ function show(req, res, next) {
     });
 }
 
+function newSkill(req, res, next) {
+    res.render('skills/new');
+}
+
+function create(req, res, next) {
+    skillsDB.create(req.body);
+    res.redirect('/skills');
+}
+
+function deleteSkill(req, res, next) {
+    skillsDB.delete(req.body);
+    res.redirect('/skills');
+}
+
+function edit(req, res, next) {
+    const skill = skillsDB.getOne(req.params.id);
+    res.render('skills/edit', {skill} );
+}
+
+function update(req, res, next) {
+    skillsDB.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
 }
--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,16 @@
 const skills = [
-    {id: 1, skill: 'coding', acquired: false},
-    {id: 2, skill: 'cleaning', acquired: true},
-    {id: 3, skill: 'driving', acquired: true},
-    {id: 4, skill: 'reading', acquired: true}
+    {id: 123456, skill: 'coding', acquired: false},
+    {id: 223456, skill: 'cleaning', acquired: true},
+    {id: 333456, skill: 'driving', acquired: true},
+    {id: 444456, skill: 'reading', acquired: true}
 ];
 
 module.exports = {
     getAll, 
-    getOne
+    getOne,
+    create,
+    delete: deleteSkill,
+    update
 };
 
 function getAll() {
@@ -16,4 +19,21 @@ function getAll() {
 
 function getOne(id){
     return skills.find(skill => skill.id === parseInt(id));
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.acquired = false;
+    skills.push(skill);
+}
+
+function deleteSkill(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id))
+    skills.splice(idx , 1);
+}
+
+
+function update(id, skill) {
+    const skillObj = skills.find(s => s.id === parseInt(id));
+    Object.assign(skillObj, skill)
 }
